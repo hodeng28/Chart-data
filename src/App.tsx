5de4d8caab772a6a0d64542f725433f915604077
hodeng28 <ghepd28@gmail.com>
2023-11-21 13:43:09 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Chart from "./Components/Chart";
+
+const App = () => {
+  return (
+    <div>
+      <Chart />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Chart.tsx
@@ -0,0 +1,61 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Chart = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "차트 연습",
+      },
+    },
+  };
+
+  const labels = ["1월", "3월", "5월", "7월", "9월", "11월"];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "다홍",
+        data: [10, 20, 30, 40, 50, 60, 70],
+        borderColor: "rgb(255, 99, 100)",
+        backgroundColor: "rgba(255, 99, 100, 0.5)",
+      },
+      {
+        label: "연두",
+        data: [2, 3, 4, 5, 4, 7, 8],
+        borderColor: "rgb(53, 162, 100)",
+        backgroundColor: "rgba(53, 162,100, 0.5)",
+      },
+    ],
+  };
+  return (
+    <>
+      <Bar data={data} />
+    </>
+  );
+};
+
+export default Chart;
