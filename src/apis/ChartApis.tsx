b3795bce1f92d33fd1440f2dfd49580a6a27ecac
hodeng28 <ghepd28@gmail.com>
2023-11-23 12:02:22 +0900

    fix: 배포용 mock data 변경

--- src/apis/ChartApis.tsx
+++ src/apis/ChartApis.tsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_CHARTS;
 
 export const fetchChart1Data = async () => {
-  const response = await axios.get<Chart1Data>(`${BASE_URL}/chart1`);
+  const response = await axios.get(`${BASE_URL}`);
 
-  return response.data;
+  return response.data.chart1;
 };
 
 export const fetchChart2Data = async () => {
-  const response = await axios.get<Chart2Data>(`${BASE_URL}/chart2`);
+  const response = await axios.get(`${BASE_URL}`);
 
-  return response.data;
+  return response.data.chart2;
 };
