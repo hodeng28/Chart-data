ef8b1f4246a8a86e87b06d36d68ac6db01c50dee
hodeng28 <ghepd28@gmail.com>
2023-11-23 12:05:26 +0900

    fix: 배포용 mock data 변경

--- src/apis/ChartApis.tsx
+++ src/apis/ChartApis.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_CHARTS;
+const BASE_URL =
+  'https://gist.githubusercontent.com/hodeng28/f1fbf6d6e342e67282d2f35646ed42ad/raw/52dfd27c08146969ed9ff79ef6eaef263dc17a73/gistfile1.txt';
 
 export const fetchChart1Data = async () => {
   const response = await axios.get(`${BASE_URL}`);
