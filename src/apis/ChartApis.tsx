72abd7c311de0ad69601eb7caece44e3e6724d77
hodeng28 <ghepd28@gmail.com>
2023-11-23 10:36:29 +0900

    fix: 패키지 변경

--- src/apis/ChartApis.tsx
+++ src/apis/ChartApis.tsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_CHARTS;
+
 export const fetchChart1Data = async () => {
-  const response = await axios.get<Chart1Data>('http://localhost:3001/chart1');
+  const response = await axios.get<Chart1Data>(`${BASE_URL}/chart1`);
 
   return response.data;
 };
 
 export const fetchChart2Data = async () => {
-  const response = await axios.get<Chart2Data>('http://localhost:3001/chart2');
+  const response = await axios.get<Chart2Data>(`${BASE_URL}/chart2`);
 
   return response.data;
 };
