b9eb5faab2fb29a908b337b0a6121135bf6c2766
hodeng28 <ghepd28@gmail.com>
2023-11-23 12:30:48 +0900

    add: 주석

--- src/apis/ChartApis.tsx
+++ src/apis/ChartApis.tsx
@@ -15,3 +15,15 @@ export const fetchChart2Data = async () => {
 
   return response.data.chart2;
 };
+
+// export const fetchChart1Data = async () => {
+//   const response = await axios.get<Chart1Data>("http://localhost:3001/chart1");
+
+//   return response.data;
+// };
+
+// export const fetchChart2Data = async () => {
+//   const response = await axios.get<Chart2Data>("http://localhost:3001/chart2");
+
+//   return response.data;
+// };
